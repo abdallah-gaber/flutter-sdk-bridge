c1a0c8e76731733a1c6a8649aef70a183b120bbe
abdallah-gaber <42086708+abdallah-gaber@users.noreply.github.com>
2022-10-22 12:35:48 +0200

    fix typo

--- lib/flutter_paytabs_bridge.dart
+++ lib/flutter_paytabs_bridge.dart
@@ -160,7 +160,7 @@ class FlutterPaytabsBridge {
   }
 
   static Future<String> handleImagePath(String path) async {
-    var bytes = await File(fullPath).readAsStringSync();
+    var bytes = await File(path).readAsStringSync();
     String dir = (await getApplicationDocumentsDirectory()).path;
     var imageName = path.split("/").last;
     String logoPath = '$dir/$imageName';
